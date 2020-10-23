6a7c9894bc56c807a4e456c299aed4eca92bd12e
Richard Price <mrspondooli@gmail.com>
2020-10-23 15:14:24 +0100

    inital commit

---
+++ SwiftUILocalRestaraunts/SwiftUILocalRestarauntsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILocalRestarauntsApp.swift
+//  SwiftUILocalRestaraunts
+//
+//  Created by Richard Price on 23/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILocalRestarauntsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
